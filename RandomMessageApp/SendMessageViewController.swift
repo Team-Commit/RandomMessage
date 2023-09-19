28b75a5ebadb81c7164c526909ba475163b652a9
suojae3 <suojae3@gmail.com>
2023-09-19 09:25:58 +0900

    setting

---
+++ RandomMessageApp/SendMessageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SendMessageView.swift
+//  RandomMessageApp
+//
+//  Created by ㅣ on 2023/09/18.
+//
+
+import UIKit
+
+
+//MARK: - Properties
+class SendMessageViewController: UIViewController {
+    
+}
+
+
+
+//MARK: - View Cycle
+
+extension SendMessageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        
+        
+    }
+}
